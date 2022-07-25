a3fe7419767bb675380a8b6f02c5bb34d0ecf202
Augusto <jesus_augga_99@icloud.com>
2022-07-25 12:05:38 -0500

    add validations for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :preparation_time, :cooking_time, numericality: { greater_than: 0, only_float: true }
 end

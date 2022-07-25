d5da6780b6fb36362e1d4abcfd683a0296c862aa
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-25 11:43:56 -0400

    Setup CanCanCan

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.is? :admin
+      can :manage, User
+      can :manage, Food
+      can :manage, Recipe
+    else
+      can :manage, User, user: user
+      can :manage, Food, user: user
+      can :manage, Recipe, user: user
+  end
+end

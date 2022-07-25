a4d69fcf223b2f25b52b751a3629a1e79edb463e
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-25 11:51:35 -0400

    Setup Linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -11,6 +9,7 @@ class Ability
     else
       can :manage, User, user: user
       can :manage, Food, user: user
-      can :manage, Recipe, user: user
+      can :manage, Recipe, user:
+    end
   end
 end

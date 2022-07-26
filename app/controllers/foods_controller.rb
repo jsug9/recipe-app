847fb3df82bb9a2e93e7ee31c145ebd767ca634e
Augusto <jesus_augga_99@icloud.com>
2022-07-26 12:37:53 -0500

    add new and create to food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,6 +5,20 @@ class FoodsController < ApplicationController
 
   def show; end
 
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to food_url(@food)
+    else
+      render :new
+    end
+  end
+
   private
 
   def food_params

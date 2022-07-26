1af41ef9da8d765dc9b1318f791720213c3fae65
Augusto <jesus_augga_99@icloud.com>
2022-07-26 11:16:53 -0500

    add index and show for foodcontroller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,14 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @foods = @user.foods
+  end
+
+  def show; end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end

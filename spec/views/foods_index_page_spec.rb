f8c31a4cde41400d9cda505a0286fa7be00a2e42
Augusto <jesus_augga_99@icloud.com>
2022-07-28 11:34:18 -0500

    add test for food index page

---
+++ spec/views/foods_index_page_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Index Page', type: :feature do
+  before(:each) do
+    @user = User.new(
+      name: "Augusto",
+      email: "augusto@dev.com",
+      password: "123456",
+      password_confirmation: "123456",
+      confirmed_at: Time.now,
+      role: "admin")
+    current_user = @user
+    @user.skip_confirmation!
+    @user.save!
+    sign_in @user
+    (1..5).each do |id|
+      Food.create(
+        name: "Food #{id}",
+        measurement_unit: "kgs",
+        price: 2.5,
+        quantity: 1000, 
+        user_id: @user.id
+      )
+    end
+    visit foods_path
+  end
+
+  it 'should have an add food button' do
+    expect(page).to have_link('Add Food')
+  end
+end

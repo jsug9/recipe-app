9bd0ac7ebce91a809d17517eb3e426de98e2b075
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-28 12:53:55 -0400

    add recipe_food model test

---
+++ spec/models/recipe_food_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe RecipeFood, type: :model do
+  before(:each) do
+    user = User.create(name: 'Augusto', email: 'augusto@dev.com', password: '123456', password_confirmation: '123456',
+                       confirmed_at: Time.now, role: 'admin')
+    recipe = Recipe.create(name: 'tasty', preparation_time: 1.0, cooking_time: 1.0,
+                            description: 'Very tasty dish description', user_id: user.id, public: true)
+    food = Food.create(name: "Carrot", measurement_unit: "kgs", price: 2.5, quantity: 1000, user_id: user.id)
+    @recipe_food = RecipeFood.create(quantity: 2, recipe_id: recipe.id, food_id: food.id)
+  end
+
+  it 'Recipe should be valid' do
+    expect(@recipe_food.food).to be_valid
+  end
+end

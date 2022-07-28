90add463ded0c469436b2ea70237c381ba68c10d
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-28 12:32:33 -0400

    add controller tests

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
   before (:each) do
-    @user = User.new(name: "Augusto", email: "augusto@dev.com", password: "123456", password_confirmation: "123456", confirmed_at: Time.now, role: "admin")
+    @user = User.create(name: "Augusto", email: "augusto@dev.com", password: "123456", password_confirmation: "123456", confirmed_at: Time.now, role: "admin")
     current_user = @user
     @user.skip_confirmation!
     @user.save!
     sign_in @user
-
-    (1..3).each do |a| 
-      a += 1.0 
-      recipe = Recipe.create(name: "#{a}", preparation_time: a, cooking_time: a, description: "Very tasty dish description", user_id: @user.id, public: true)
+    (1..3).each do |a|  
+      Recipe.create(name: "cool", preparation_time: 1.0, cooking_time: 1.0, description: "Very tasty dish description", user_id: @user.id, public: true)
     end
   end
 
@@ -19,11 +17,30 @@ RSpec.describe 'Recipes', type: :request do
       get recipes_path
       expect(response).to have_http_status(200)
     end
+
+    it 'should render the correct text' do
+      get recipes_path
+      expect(response.body).to include("Recipes")
+    end
+  end
+
+  describe 'GET /show' do
+    it 'response for show action status is correct' do
+      recipe = Recipe.first
+      get recipe_path(recipe.id)
+      expect(response).to have_http_status(200)
+    end
   end
 
   it 'tests that the recipes of the user are found' do
     
     get recipes_path
     expect(response).to render_template 'index'
+  end
+
+  it 'should render the correct text' do
+    recipe = Recipe.first
+    get recipe_path(recipe.id)
+    expect(response.body).to include("cool")
   end
 end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe Recipe, type: :model do
+  before (:each) do
+    user = User.create(name: "Augusto", email: "augusto@dev.com", password: "123456", password_confirmation: "123456", confirmed_at: Time.now, role: "admin")
+    @recipe = Recipe.create(name: "tasty", preparation_time: 1.0, cooking_time: 1.0, description: "Very tasty dish description", user_id: user.id, public: true)
+  end
+
+  it 'Recipe should be valid' do
+    expect(@recipe).to be_valid
+  end
+
+end

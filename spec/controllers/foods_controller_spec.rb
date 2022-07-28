2ee33b807b914f62c25d415a7ad13a5858fe4d76
Augusto <jesus_augga_99@icloud.com>
2022-07-28 10:33:51 -0500

    add tests for food

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -1,32 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :system do
+RSpec.describe 'Foods', type: :request do
   before(:each) do
-    User.destroy_all
     @user = User.new(
-      name: 'Aaron',
-      email: 'aaron@test.com',
-      password: 'password',
-      password_confirmation: 'password',
-      role: 'admin'
-    )
+      name: "Augusto",
+      email: "augusto@dev.com",
+      password: "123456",
+      password_confirmation: "123456",
+      confirmed_at: Time.now,
+      role: "admin")
+    current_user = @user
     @user.skip_confirmation!
     @user.save!
-    Food.destroy_all
+    sign_in @user
     food = Food.create(name: "Carrot", measurement_unit: "kgs", price: 2.5, quantity: 1000, user_id: @user.id)
   end
 
   describe 'GET /index' do
-    before :each do
-      visit new_user_session_path
-      fill_in 'Email', with: 'aaron@test.com'
-      fill_in 'Password', with: 'password'
-      click_button 'Log in'
-    end
+    before { get foods_path }
 
     it 'returns a 200 status code' do
-      visit foods_path 
-      expect(page).to have_http_status(200)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'should render the correct text' do
+      expect(response.body).to include('Carrot')
     end
   end
 end
